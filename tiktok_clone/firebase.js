ff0c0e55e37bfdf6cec788bc86a083059f21c31f
fouad benayad <fouad.benayad@bioreal.local>
2021-10-18 16:42:11 +0100

    Started the project

---
+++ tiktok_clone/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBiuMe6l66Lc7hnqLRC_Sz1goTRLT8tRTM",
+  authDomain: "tiktok-clone-c723c.firebaseapp.com",
+  projectId: "tiktok-clone-c723c",
+  storageBucket: "tiktok-clone-c723c.appspot.com",
+  messagingSenderId: "903474984828",
+  appId: "1:903474984828:web:a676a8632da7b75fa516f9",
+  measurementId: "G-VQJ3SXDJTK"
+};
---
+++ tiktok_clone/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1></h1>
+     <div className="app__videos">
+       <Video />
+     </div>
+    </div>
+  );
+}
+
+export default App;
